4c91147f31ba63ec0d97da2405a66efd52f5a74d
root <root@localhost.localdomain>
2019-03-20 06:22:27 +0000

    19-03-19 1th commit

--- app/routers/user.go
+++ app/routers/user.go
@@ -2,7 +2,7 @@ package routers
 
 import (
 	"net/http"
-
+	. "chitchat_mvc/app/utils"
 	"github.com/julienschmidt/httprouter"
 	"chitchat_mvc/app/controllers/user"
 	"chitchat_mvc/app/controllers/common"
@@ -11,21 +11,29 @@ import (
 // New .
 func New() http.Handler {
 	router := httprouter.New()
-	
+
+	// handle static assets
+	router.ServeFiles("/static/*filepath", http.Dir(Config.Static))
+
+	// index
 	router.GET("/", common.Index)
+	// error
 	router.GET("/err", common.Err)
 
+	// defined in controllers/user/user.go
 	router.GET("/login", user.Login)
 	router.GET("/logout", user.Logout)
 	router.GET("/signup", user.Signup)
 	router.POST("/signup_account", user.SignupAccount)
 	router.POST("/authenticate", user.Authenticate)
 
+	// defined in controllers/thread/thread.go
 	router.GET("/thread/new", thread.NewThread)
 	router.POST("/thread/create", thread.CreateThread)
 	router.POST("/thread/post", thread.PostThread)
 	router.GET("/thread/read/:id", thread.ReadThread)
 
+	
 
 	return router
 }
--- main.go
+++ main.go
@@ -8,41 +8,16 @@ import (
 	"chitchat_mvc/app/routers"
 )
 
+
+
 func main() {
 	
 	Info("===", GetCurrentDirectory(),Config.DbDriverName)
-	 P("ChitChat",  Version(), "started at",  Config.Address)
+	P("ChitChat",  Version(), "started at",  Config.Address)
 
-	
+	// Initialize a router as usual
 	handler := routers.New()
-	//
-	// all route patterns matched here
-	// route handler functions defined in other files
-	//
-	/*
-	// handle static assets
-	mux := http.NewServeMux()
-	files := http.FileServer(http.Dir(  Config.Static))
-	mux.Handle("/static/", http.StripPrefix("/static/", files))
 
-	// index
-	mux.HandleFunc("/", Index)
-	// error
-	mux.HandleFunc("/err", Err)
-
-	// defined in route_auth.go
-	mux.HandleFunc("/login", Login)
-	mux.HandleFunc("/logout", Logout)
-	mux.HandleFunc("/signup", Signup)
-	mux.HandleFunc("/signup_account", SignupAccount)
-	mux.HandleFunc("/authenticate", Authenticate)
-
-	// defined in route_thread.go
-	mux.HandleFunc("/thread/new", NewThread)
-	mux.HandleFunc("/thread/create", CreateThread)
-	mux.HandleFunc("/thread/post", PostThread)
-	mux.HandleFunc("/thread/read", ReadThread)
-*/
 	// starting up the server
 	server := &http.Server{
 		Addr:            Config.Address,
